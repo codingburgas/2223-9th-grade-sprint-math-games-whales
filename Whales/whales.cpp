70fec219c1cab65f4e620fd646f76875d2326d26
Вичо Димитров Вичев <VDVichev21@codingburgas.bg>
2023-03-24 10:45:29 +0200

    Add button hover effect

--- Whales/whales.cpp
+++ Whales/whales.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1000;
+    const int screenHeight = 600;
     //gets the working directory of the photo
     const char* currentDir = GetWorkingDirectory();
     const char* mediaPath = TextFormat("%s/2223-9th-grade-sprint-math-games-whales/", currentDir);
@@ -34,21 +34,38 @@ int main() {
 
         // Draw button text
         DrawText("Play", playButton.x + 65, playButton.y + 15, 20, BLACK);
-        DrawText("Options", optionsButton.x + 50, optionsButton.y + 15, 20, BLACK);
+        DrawText("Options", optionsButton.x + 45, optionsButton.y + 15, 40, BLACK);
         DrawText("Exit", exitButton.x + 65, exitButton.y + 15, 20, BLACK);
 
-        // Check for button clicks
-        if (CheckCollisionPointRec(GetMousePosition(), playButton) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+        // Check for collision
+
+        if (CheckCollisionPointRec(GetMousePosition(), playButton))
+        {         
+            DrawRectangleRec(playButton, RED);
+            DrawText("Play", playButton.x + 65, playButton.y + 15, 20, BLACK);
+        }
+        if (CheckCollisionPointRec(GetMousePosition(), optionsButton))
+        {
+            DrawRectangleRec(optionsButton, RED);
+            DrawText("Options", optionsButton.x + 45, optionsButton.y + 15, 20, BLACK);
+        }
+        if (CheckCollisionPointRec(GetMousePosition(), exitButton))
+        {
+            DrawRectangleRec(exitButton, RED);
+            DrawText("Exit", exitButton.x + 65, exitButton.y + 15, 20, BLACK);
+        }
+        //check if the button is clicked
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
             // Play button clicked
             // Insert your code here for what happens when the Play button is clicked
         }
 
-        if (CheckCollisionPointRec(GetMousePosition(), optionsButton) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
             // Options button clicked
             // Insert your code here for what happens when the Options button is clicked
         }
 
-        if (CheckCollisionPointRec(GetMousePosition(), exitButton) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
             // Exit button clicked
             break; // Exit the loop and close the window
         }
